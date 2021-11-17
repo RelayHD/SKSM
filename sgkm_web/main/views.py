34bdbdb0464f142ec8ce5825699c054e694ad0ed
RelayHD <hd@relay-works.kr>
2021-11-17 15:29:02 +0900

    뼈대가 만들어졌다

--- sgkm_web/main/views.py
+++ sgkm_web/main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.urls.resolvers import URLPattern
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts= Post.objects.all().order_by('-created_at')
+
+    return render(
+        request,
+        'main/index.html',
+        {
+            'posts':posts,
+        }
+    )
+
+def single_post_page(request, pk):
+    post= Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'main/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
--- sgkm_web/single_pages/views.py
+++ sgkm_web/single_pages/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.urls.resolvers import URLPattern
+from .models import SinglePost
 
-# Create your views here.
+def profile(request):
+    post= SinglePost.objects.filter(tag="profile")
+
+    return render(
+        request,
+        'single_pages/template.html',
+        {
+            'post': post,
+        }
+    )
+
+def contact(request):
+    post= SinglePost.objects.filter(tag="contact")
+
+    return render(
+        request,
+        'single_pages/template.html',
+        {
+            'post': post,
+        }
+    )
