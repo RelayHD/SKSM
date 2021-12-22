21a928e51288775e4646eb6c747b1bc2c3ddf557
relayworks <94586926+relayworks@users.noreply.github.com>
2021-12-22 21:16:45 +0900

    css기타등등 수정

---
+++ main/templatetags/check_empty.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def check_empty(value):
+    content = value # 전달된 value 객체의 content 멤버변수를 가져온다.
+    if value.paragraph_1 == value.paragraph_2 == value.paragraph_3 == value.paragraph_4:
+        return False
+    else:
+        return True
